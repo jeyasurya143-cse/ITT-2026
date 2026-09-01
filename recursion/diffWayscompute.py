ccc096adc0a3db222899762369431ef5d6a21dfc
JEYASURYA <jeyasurya887085@gmail.com>
2026-09-01 15:54:14 +0530

    Add diffWaysToCompute method for expression evaluation

---
+++ recursion/diffWayscompute.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def diffWaysToCompute(self, expression):
+        if expression.isdigit():
+            return [int(expression)]
+        res = []
+        for i, c in enumerate(expression):
+            if c in '+-*':
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                for l in left:
+                    for r in right:
+                        if c == '+':
+                            res.append(l + r)
+                        elif c == '-':
+                            res.append(l - r)
+                        elif c == '*':
+                            res.append(l * r)
+        return res
